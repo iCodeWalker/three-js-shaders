542d35b668b6c0baee156982776e8106089f3c47
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-03-16 00:23:02 +0530

    added : RawShaderMaterial

--- src/script.js
+++ src/script.js
@@ -26,7 +26,32 @@ const textureLoader = new THREE.TextureLoader();
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
 // Material
-const material = new THREE.MeshBasicMaterial();
+// Replace 'MeshBasicMaterial' with 'RawShaderMaterial'
+// const material = new THREE.RawShaderMaterial();
+
+// we will use the vertexShader and fragmentShader properties to provide the shaders
+const material = new THREE.RawShaderMaterial({
+  vertexShader: `
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+
+    attribute vec3 position;
+
+    void main()
+    {
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    precision mediump float;
+
+    void main()
+    {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  `,
+});
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
