f9c7e893c9c6973f4e4b9c8932df83f3e491c66a
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-03-27 22:49:07 +0530

    added : separating matrix into individual postions

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -14,8 +14,23 @@ void main()
 {
     // Shorter version where the viewMatrix and the modelMatrix are combined into a modelViewMatrix 
     // gl_Position = projectionMatrix * modelViewMatrix  * vec4(position, 1.0);
+    
+    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    // ######## Separating each matrix ###########
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Now we can manipulate the shader position.
+    // modelPosition.x += 1.0;
+    // Now we can create a wave.
+
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPostion = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPostion;
 
 
     // #### Don't do it like this
